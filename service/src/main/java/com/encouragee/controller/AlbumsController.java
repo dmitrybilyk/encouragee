1efe91d414219e1501d986d2c30786f821a81bc4
dmytro <dmitry.b@scopicsoftware.com>
2022-09-05 21:15:17 +0300

    added keycloak authentication

--- service/src/main/java/com/encouragee/controller/AlbumsController.java
+++ service/src/main/java/com/encouragee/controller/AlbumsController.java
@@ -1,6 +1,16 @@
 package com.encouragee.controller;
 
 import com.encouragee.model.Album;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +19,33 @@ import java.util.Collections;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class AlbumsController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/albums")
-    public String getAlbums(Model model) {
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("authentication - " + authentication);
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient oAuth2AuthorizedClient =
+                oAuth2AuthorizedClientService.loadAuthorizedClient(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(),
+                oAuth2AuthenticationToken.getName());
+
+        String accessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
+
+        log.info("access token - " + accessToken);
+
+        log.info("Principal - " + principal);
+
+        OidcIdToken idToken = principal.getIdToken();
+        String idTokenString = idToken.getTokenValue();
+        log.info("IdTokenValue - " + idTokenString);
+
         List<Album> albumList = Collections.singletonList(
                 Album.builder().albumTitle("my title").build());
         model.addAttribute("albums", albumList);
